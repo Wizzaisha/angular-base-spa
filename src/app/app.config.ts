f0553a8587e697048ce586bac6308cce8aab30c9
ever <everramirez.vortex@gmail.com>
2025-05-29 19:08:55 -0500

    new commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,53 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+
+import {
+  DialogService,
+  DynamicDialogRef,
+  DynamicDialogConfig,
+} from 'primeng/dynamicdialog';
+import { environment } from '../environments/environment.development';
+import StylePreset from './shared/utils/style-presets/style-preset';
+import { apiUrlInterceptor } from './shared/interceptors/api-url.interceptor';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore(),
+    provideAnimations(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: StylePreset,
+        options: {
+          darkModeSelector: '.my-app-dark',
+        },
+      },
+    }),
+    provideEffects(),
+    provideHttpClient(withFetch(), withInterceptors([apiUrlInterceptor])),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !environment.production,
+      autoPause: true,
+    }),
+    DialogService,
+    DynamicDialogRef,
+    DynamicDialogConfig,
+  ],
 };
---
+++ src/app/pages/module1/module1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-module1',
+  imports: [],
+  templateUrl: './module1.component.html',
+  styleUrl: './module1.component.css'
+})
+export class Module1Component {
+
+}
